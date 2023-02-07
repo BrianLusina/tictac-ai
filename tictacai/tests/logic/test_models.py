dc0032f5868a952b111f72fcfb4fe86f110b1c7e
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2023-02-07 08:59:16 +0300

    feat(models): grid model
    Grid represents the Board of the game.
    The spaces will contain the Marks of each player's move.

--- tictacai/tests/logic/test_models.py
+++ tictacai/tests/logic/test_models.py
@@ -1,5 +1,5 @@
 import unittest
-from ...tictacai.logic.models import Mark
+from ...tictacai.logic.models import Mark, Grid
 
 
 class ModelsTestCases(unittest.TestCase):
@@ -14,6 +14,50 @@ class ModelsTestCases(unittest.TestCase):
         """Mark.other property retrieves the other mark from enum"""
         self.assertTrue(isinstance(Mark.CROSS, str))
         self.assertTrue(isinstance(Mark.NAUGHT, str))
+
+    def test_new_grid_has_9_empty_spaces(self):
+        """Grid instance should have 9 empty spaces"""
+        grid = Grid()
+        actual = grid.cells
+        expected = 9
+        self.assertTrue(expected, len(actual))
+
+    def test_grid_contains_a_particular_cell_combination(self):
+        """Grid instance should contain a particular cell combination"""
+        grid = Grid("XXOXO O  ")
+        actual = grid.cells
+        expected = "XXOXO O  "
+        self.assertTrue(expected, actual)
+
+    def test_grid_is_not_created_with_too_few_cells(self):
+        """Grid instance should not be created with too few cells"""
+        with self.assertRaises(ValueError) as ex:
+            Grid("XX")
+
+        expected = "Must contain 9 cells of: X, O or space"
+        self.assertEquals(expected, ex.exception.args[0])
+
+    def test_grid_is_not_created_with_invalid_characters(self):
+        """Grid instance should not be created with invalid characters"""
+        with self.assertRaises(ValueError) as ex:
+            Grid("XXOxO O  ")
+
+        expected = "Must contain 9 cells of: X, O or space"
+        self.assertEquals(expected, ex.exception.args[0])
+
+    def test_grid_can_get_the_count_of_Xs_and_Os_and_empty(self):
+        """Grid can get the correct count for Xs and Os & empty Slots"""
+        grid = Grid("OXXXXOOOX")
+        actual_x_count = grid.x_count
+        expected_x_count = 5
+        actual_o_count = grid.o_count
+        expected_o_count = 4
+        actual_empty_count = grid.empty_count
+        expected_empty_count = 0
+
+        self.assertEquals(expected_x_count, actual_x_count)
+        self.assertEquals(expected_o_count, actual_o_count)
+        self.assertEquals(expected_empty_count, actual_empty_count)
 
 
 if __name__ == "__main__":
