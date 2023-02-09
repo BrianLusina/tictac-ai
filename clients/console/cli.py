349e00cc6a083b5b7e3724b34566bad20b85a16b
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2023-02-09 06:43:42 +0300

    feat(clients): cli parse args
    Allow game to be interactive via CLI

---
+++ clients/console/cli.py
@@ -0,0 +1,10 @@
+from tictacai.game.engine import TicTacToe
+
+from .args import parse_args
+from .renderers import ConsoleRenderer
+
+
+def main() -> None:
+    player1, player2, starting_mark = parse_args()
+    tic_tac_toe = TicTacToe(playerOne=player1, playerTwo=player2, renderer=ConsoleRenderer())
+    tic_tac_toe.play(starting_mark=starting_mark)
